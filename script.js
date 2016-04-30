cab858c6cbea0a1b0bbfe9e87095c9ea3bd89b09
Karthik Varma <leovarmak@hotmail.com>
2016-04-30 09:05:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Hi ! Would you like to talk to the Support team or would you like to register for a tournament ?')
                 .then(() => 'speak');
         }
     },
